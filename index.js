94a1b9fe00da08b676ac5d35190a4fa26ab3e9c9
Ali Madooei <madooei@jhu.edu>
2020-10-20 09:50:28 -0400

    Step 7

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const noteRoutes = require("./routes/notes.js");
 const cors = require("cors");
 const helmet = require("helmet");
+const morgan = require("morgan");
 const app = express();
 const port = process.env.PORT || 4567;
 
 app.use(cors());
 app.use(helmet());
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(noteRoutes);
 
